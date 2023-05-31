5f35b1e1be5c1a9ee474436869f39001a9f3c928
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-05-31 21:28:12 +0200

    minor style fixes in SearchRecipes component

--- client/src/components/Recipes/SearchRecipes/SearchRecipes.jsx
+++ client/src/components/Recipes/SearchRecipes/SearchRecipes.jsx
@@ -10,12 +10,12 @@ const SearchRecipes = () => {
       <input
         type="text"
         placeholder="Search recipes"
-        className="outline-none border-none xs:w-[50vw] md:w-[60vw] bg-inherit xs:h-[1.5rem] md:h-[3rem]"
+        className="outline-none border-none xs:w-[50vw] md:w-[60vw] bg-inherit"
       />
 
       {/* Search Icon - Container */}
       <button
-        className="xs:py-[4px] md:py-[6px] xs:px-[.7rem] md:pl-[2rem] flex items-center gap-2 text-gray-800 bg-color_orange rounded-full p-4 text-xl font-semibold hover:bg-orange-500"
+        className="xs:py-[3px] md:py-[4px] xs:px-[.7rem] md:pl-[2rem] flex items-center gap-2 text-gray-800 bg-color_orange rounded-full p-4 text-xl font-semibold hover:bg-orange-500"
         onClick={() => {}}
       >
         Search
