1f7293a7b51f87d98384b41bf5b170860184804b
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-03 21:49:46 +0200

    adding Authorization middleware in Frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,6 +3,16 @@ import axios from "axios";
 // axios instant
 const API = axios.create({ baseURL: "http://localhost:8080" });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profle")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
+
 export const fetchPosts = () => API.get("/posts");
 
 export const fetchBreakfastPosts = () => API.get(`/posts/breakfast`);
