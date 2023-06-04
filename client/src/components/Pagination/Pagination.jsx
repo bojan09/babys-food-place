d04c4707593bb9e3523ce8bad99b986f322379c2
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-04 12:51:05 +0200

    fixed minor style bugs in Navbar & Home components

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,29 +1,5 @@
 import React from "react";
 
-const items = [
-  {
-    id: 1,
-    title: "Back End Developer",
-    department: "Engineering",
-    type: "Full-time",
-    location: "Remote",
-  },
-  {
-    id: 2,
-    title: "Front End Developer",
-    department: "Engineering",
-    type: "Full-time",
-    location: "Remote",
-  },
-  {
-    id: 3,
-    title: "User Interface Designer",
-    department: "Design",
-    type: "Full-time",
-    location: "Remote",
-  },
-];
-
 const Pagination = () => {
   return (
     <div>
